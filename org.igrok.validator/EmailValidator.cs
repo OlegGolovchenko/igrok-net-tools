5a110e6b2709c6f368e5bcda13aedabadd46933d
Oleg <igrok_be@hotmail.com>
2022-08-17 17:17:34 +0200

    version 1.1.10.3 (#16)
    * removed useless yaml file
    
    * updated package with license
    
    * removed unneeded lib folder and updated package to newest IgnActivation
    
    * updated to latest IGNActivation client
    
    * Update README.md
    
    * updated to latest ignactivationclient
    updated project url in package
    
    * added some more vat validations and updated activation client
    
    * updated version number of package
    
    * updated ignactivator client
    
    * added icon
    
    * minor optimization of code and unittests
    
    * updated activator package to reoptimise unittests to stop fails from failed activation
    
    * updated with latest version of IGNActivator
    
    * Update README.md
    
    * moved to new activation client
    
    * updated to fixed version of activator
    
    * updated activator to fix build problems
    
    * attempt to fix build errors
    
    * attempt to fix unittests and build for validator by switching to offline activation
    
    * fixed incorrect build
    
    * moved to use of new activation client and updated version of .net core
    
    * added nunit console runner packages
    
    * updated to latest ignactivator
    
    * corrected ierland vat number
    removed some unneeded methods
    reused same files for netcore and netfx tests
    added tests for Ierland and Italy
    
    * try to fix unittests on azure
    
    * another fix for unittests
    
    * removed some unneeded nuget packages for tests
    
    * fixed unittest problem
    
    * fixed spain vat format rules
    
    * added unittests for LT LV vat numbers
    
    * updated packages
    added preamble to source files to mark what is under gpl

--- org.igrok.validator/EmailValidator.cs
+++ org.igrok.validator/EmailValidator.cs
@@ -1,4 +1,21 @@
-﻿using IGNActivation.Client;
+﻿//###########################################################################################################
+//    Igrok-Net validators
+//    Copyright(C) 2017  Oleg Golovchenko
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//###########################################################################################################
+using IGNActivation.Client;
 using System;
 using System.Text.RegularExpressions;
 #if !NET40
